1ae2332fd06a49dbc65142e0cb38ba96bbdf5c27
iw0rm3r <iw0rm3r@gmail.com>
2019-04-06 02:14:43 +0300

    Добавьте файлы проекта.

---
+++ DownAllPics/MainWindow.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace DownAllPics
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.label1 = new System.Windows.Forms.Label();
+            this.srcPageUrlTB = new System.Windows.Forms.TextBox();
+            this.useProxyCB = new System.Windows.Forms.CheckBox();
+            this.proxyAddrTB = new System.Windows.Forms.TextBox();
+            this.useCookieCB = new System.Windows.Forms.CheckBox();
+            this.cookieTB = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.logTB = new System.Windows.Forms.TextBox();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.clearSrcPageBtn = new System.Windows.Forms.Button();
+            this.clearProxyAddrBtn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.localDirTB = new System.Windows.Forms.TextBox();
+            this.pickDirBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 58);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(302, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Адрес исходной страницы со ссылками на пикчи:";
+            // 
+            // srcPageUrlTB
+            // 
+            this.srcPageUrlTB.Location = new System.Drawing.Point(12, 78);
+            this.srcPageUrlTB.Name = "srcPageUrlTB";
+            this.srcPageUrlTB.Size = new System.Drawing.Size(451, 25);
+            this.srcPageUrlTB.TabIndex = 1;
+            // 
+            // useProxyCB
+            // 
+            this.useProxyCB.AutoSize = true;
+            this.useProxyCB.Location = new System.Drawing.Point(12, 352);
+            this.useProxyCB.Name = "useProxyCB";
+            this.useProxyCB.Size = new System.Drawing.Size(161, 21);
+            this.useProxyCB.TabIndex = 2;
+            this.useProxyCB.Text = "Использовать прокси:";
+            this.useProxyCB.UseVisualStyleBackColor = true;
+            this.useProxyCB.CheckedChanged += new System.EventHandler(this.useProxyCB_CheckedChanged);
+            // 
+            // proxyAddrTB
+            // 
+            this.proxyAddrTB.Enabled = false;
+            this.proxyAddrTB.Location = new System.Drawing.Point(12, 379);
+            this.proxyAddrTB.Name = "proxyAddrTB";
+            this.proxyAddrTB.Size = new System.Drawing.Size(221, 25);
+            this.proxyAddrTB.TabIndex = 3;
+            // 
+            // useCookieCB
+            // 
+            this.useCookieCB.AutoSize = true;
+            this.useCookieCB.Location = new System.Drawing.Point(12, 109);
+            this.useCookieCB.Name = "useCookieCB";
+            this.useCookieCB.Size = new System.Drawing.Size(238, 21);
+            this.useCookieCB.TabIndex = 4;
+            this.useCookieCB.Text = "Подставлять cookie в HTTP запрос:";
+            this.useCookieCB.UseVisualStyleBackColor = true;
+            this.useCookieCB.CheckedChanged += new System.EventHandler(this.useCookieCB_CheckedChanged);
+            // 
+            // cookieTB
+            // 
+            this.cookieTB.Enabled = false;
+            this.cookieTB.Location = new System.Drawing.Point(12, 136);
+            this.cookieTB.Multiline = true;
+            this.cookieTB.Name = "cookieTB";
+            this.cookieTB.Size = new System.Drawing.Size(476, 210);
+            this.cookieTB.TabIndex = 5;
+            this.cookieTB.KeyDown += new System.Windows.Forms.KeyEventHandler(this.cookieTB_KeyDown);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 407);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Лог:";
+            // 
+            // logTB
+            // 
+            this.logTB.BackColor = System.Drawing.SystemColors.Window;
+            this.logTB.Location = new System.Drawing.Point(12, 427);
+            this.logTB.Multiline = true;
+            this.logTB.Name = "logTB";
+            this.logTB.ReadOnly = true;
+            this.logTB.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.logTB.Size = new System.Drawing.Size(476, 246);
+            this.logTB.TabIndex = 7;
+            this.logTB.WordWrap = false;
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(264, 352);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(224, 52);
+            this.startBtn.TabIndex = 9;
+            this.startBtn.Text = "Запустить загрузку";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 680);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(476, 23);
+            this.progressBar.TabIndex = 10;
+            // 
+            // clearSrcPageBtn
+            // 
+            this.clearSrcPageBtn.Location = new System.Drawing.Point(463, 78);
+            this.clearSrcPageBtn.Name = "clearSrcPageBtn";
+            this.clearSrcPageBtn.Size = new System.Drawing.Size(25, 25);
+            this.clearSrcPageBtn.TabIndex = 11;
+            this.clearSrcPageBtn.Text = "❌";
+            this.clearSrcPageBtn.UseVisualStyleBackColor = true;
+            this.clearSrcPageBtn.Click += new System.EventHandler(this.clearSrcPageBtn_Click);
+            // 
+            // clearProxyAddrBtn
+            // 
+            this.clearProxyAddrBtn.Enabled = false;
+            this.clearProxyAddrBtn.Location = new System.Drawing.Point(233, 379);
+            this.clearProxyAddrBtn.Name = "clearProxyAddrBtn";
+            this.clearProxyAddrBtn.Size = new System.Drawing.Size(25, 25);
+            this.clearProxyAddrBtn.TabIndex = 12;
+            this.clearProxyAddrBtn.Text = "❌";
+            this.clearProxyAddrBtn.UseVisualStyleBackColor = true;
+            this.clearProxyAddrBtn.Click += new System.EventHandler(this.clearProxyAddrBtn_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(281, 17);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Адрес конечной директории для сохранения:";
+            // 
+            // localDirTB
+            // 
+            this.localDirTB.Location = new System.Drawing.Point(12, 30);
+            this.localDirTB.Name = "localDirTB";
+            this.localDirTB.Size = new System.Drawing.Size(451, 25);
+            this.localDirTB.TabIndex = 14;
+            // 
+            // pickDirBtn
+            // 
+            this.pickDirBtn.Location = new System.Drawing.Point(463, 30);
+            this.pickDirBtn.Name = "pickDirBtn";
+            this.pickDirBtn.Size = new System.Drawing.Size(25, 25);
+            this.pickDirBtn.TabIndex = 15;
+            this.pickDirBtn.Text = "...";
+            this.pickDirBtn.UseVisualStyleBackColor = true;
+            this.pickDirBtn.Click += new System.EventHandler(this.pickDirBtn_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 713);
+            this.Controls.Add(this.pickDirBtn);
+            this.Controls.Add(this.localDirTB);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.clearProxyAddrBtn);
+            this.Controls.Add(this.clearSrcPageBtn);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.logTB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cookieTB);
+            this.Controls.Add(this.useCookieCB);
+            this.Controls.Add(this.proxyAddrTB);
+            this.Controls.Add(this.useProxyCB);
+            this.Controls.Add(this.srcPageUrlTB);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DownAllPics 0.3";
+            this.HelpButtonClicked += new System.ComponentModel.CancelEventHandler(this.MainWindow_HelpButtonClicked);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainWindow_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox srcPageUrlTB;
+        private System.Windows.Forms.CheckBox useProxyCB;
+        private System.Windows.Forms.TextBox proxyAddrTB;
+        private System.Windows.Forms.CheckBox useCookieCB;
+        private System.Windows.Forms.TextBox cookieTB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox logTB;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button clearSrcPageBtn;
+        private System.Windows.Forms.Button clearProxyAddrBtn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox localDirTB;
+        private System.Windows.Forms.Button pickDirBtn;
+    }
+}
+
---
+++ DownAllPics/MainWindow.cs
@@ -0,0 +1,285 @@
+﻿using DownAllPics.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DownAllPics
+{
+    public partial class MainWindow : Form
+    {
+        private static string[] picExt = { @".gif", @".png", @".jpg", @".jpeg" };
+        private static Regex hrefRegex = new Regex("href\\s*=\\s*(?:[\"'](?<1>[^\"']*)[\"']|(?<1>\\S+))",
+            RegexOptions.IgnoreCase | RegexOptions.Compiled, TimeSpan.FromSeconds(1));
+        private static FolderBrowserDialog folderBrowserDlg = new FolderBrowserDialog();
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Загрузить значения текстовых полей из файла настроек
+            useProxyCB.Checked = Properties.Settings.Default.useProxy;
+            useCookieCB.Checked = Properties.Settings.Default.useCookie;
+            srcPageUrlTB.Text = Properties.Settings.Default.srcPageUrl;
+            proxyAddrTB.Text = Properties.Settings.Default.proxyAddr;
+            cookieTB.Text = Properties.Settings.Default.cookieStr;
+            localDirTB.Text = Properties.Settings.Default.localDir;
+        }
+        private void MainWindow_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //Сохранить значения текстовых полей в файл настроек
+            Properties.Settings.Default.useProxy = useProxyCB.Checked;
+            Properties.Settings.Default.useCookie = useCookieCB.Checked;
+            Properties.Settings.Default.srcPageUrl = srcPageUrlTB.Text;
+            Properties.Settings.Default.proxyAddr = proxyAddrTB.Text;
+            Properties.Settings.Default.cookieStr = cookieTB.Text;
+            Properties.Settings.Default.localDir = localDirTB.Text;
+
+            Settings.Default.Save();
+        }
+
+        private void DownloadPictures(string srcPageUrl, bool useProxy,
+            string proxyAddr, bool useCookie, string cookieStr, string localDir)
+        {
+            //Инициализация переменных
+            WebClient webClient = new WebClient();
+            Uri srcPageUri = new Uri(srcPageUrl);
+            List<string> collectedUrls = new List<string>();
+            int successfulDownloads = 0;
+            string srcPage = @"";
+            string currExt = @"";
+
+            webClient.Encoding = Encoding.UTF8;
+            webClient.Headers.Add(@"user-agent", @"Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36");
+            if (useProxy)  webClient.Proxy = new WebProxy(proxyAddr);
+            if (useCookie) webClient.Headers.Add(HttpRequestHeader.Cookie, cookieStr);
+
+            //Загрузить исходную страницу как строку
+            try
+            {
+                srcPage = webClient.DownloadString(srcPageUri);
+            }
+            catch (Exception ex)
+            {
+                /*В случае ошибки загрузки исходной страницы вывести сообщение с ошибкой и
+                остановить исполнение метода*/
+                MessageBox.Show(ex.Message, @"Ошибка загрузки исходной страницы",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            //Извлечь все значения href из исходной страницы
+            MatchCollection hrefMatches = hrefRegex.Matches(srcPage);
+            srcPage = @""; //Очистить строку с исходной страницей
+            foreach (Match match in hrefMatches)
+            {
+                string currUrl = Convert.ToString(match.Groups[1]);
+                //Проверить, что текущая ссылка ведёт на пикчу
+                try
+                {
+                    currExt = Path.GetExtension(currUrl);
+                }
+                catch { }
+                if (picExt.Contains(currExt))
+                {
+                    /*Проверить, не относительна ли текущая ссылка, и если так -
+                    конвертировать в абсолютную*/
+                    if (!Uri.IsWellFormedUriString(currUrl, UriKind.Absolute)) {
+                        currUrl = new Uri(srcPageUri, currUrl).AbsoluteUri;
+                    }
+                    //Добавить ссылку в список для скачивания
+                    collectedUrls.Add(currUrl);
+                }
+            }
+            hrefMatches = null; //Удалить коллекцию значений href
+
+            /*Если не извлечено ни одной ссылки на пикчу - вывести сообщение и прервать
+            выполнение метода*/
+            if (collectedUrls.Count == 0)
+            {
+                
+                MessageBox.Show(@"На указанной странице не найдено ни одной ссылки на пикчу",
+                    @"Ошибка загрузки пикч", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            //Удалить дубликаты из списка извлечённых ссылок на пикчи
+            List<string> urlsToDownload = collectedUrls.Distinct().ToList();
+            collectedUrls = null; //Удалить список собранных пикч
+
+            //Сохранить и вывести число извлечённых из исходной страницы ссылок на пикчи
+            int totalUrls = urlsToDownload.Count;
+            WriteLineToLog($"С указанной web-страницы извлечено {totalUrls} ссылок на пикчи");
+
+            //Создать целевую директорую, если она не существует
+            localDir += $"{srcPageUri.Host} ({DateTime.Now.ToString("yyyy.MM.dd HH-mm")})\\";
+            Directory.CreateDirectory(localDir);
+
+            //Цикл загрузки файлов из сформированного списка
+            for (int i = 0; i < totalUrls; i++)
+            {
+                WriteLineToLog($"Качаю файл #{i + 1}: {urlsToDownload.ElementAt(i)}");
+                /*Попытаться скачать файл по текущей ссылке и в случае удачи увеличить
+                счётчик успешных загрузок*/
+                if (DownloadToDisk(webClient, urlsToDownload.ElementAt(i), localDir) == true)
+                    successfulDownloads++;
+                //Обновить значение статусбара с хаком против тормозов
+                int currPercent = (int)(((i + 1.0) / totalUrls) * 100);
+                if (currPercent < 100)
+                {
+                    progressBar.Value = currPercent + 1;
+                }
+                progressBar.Value = currPercent;
+            }
+
+            //Вывести отчёт о выполненной работе
+            double downPercent = Math.Round((successfulDownloads * 1.0 / totalUrls) * 100);
+            MessageBox.Show($"Загружено {successfulDownloads} из "+
+                $"{totalUrls} пикч ({downPercent}%)", @"Задание выполнено",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private bool DownloadToDisk(WebClient wc, string url, string dir)
+        {
+            bool successful = true;
+            string fileName = Path.GetFileName(url);
+            string localPath = $"{dir}\\{fileName}";
+
+            try
+            {
+                wc.DownloadFile(url, localPath);
+            }
+            catch (Exception ex)
+            {
+                successful = false;
+                WriteLineToLog(ex.Message);
+            }
+            return successful;
+        }
+
+        private void WriteLineToLog(string message)
+        {
+            string currTime = DateTime.Now.ToString(@"HH:mm:ss");
+            logTB.AppendText($"{currTime}: {message}\n");
+            logTB.SelectionStart = logTB.Text.Length;
+            logTB.ScrollToCaret();
+        }
+
+        private void PartialInputError(string message)
+        {
+            MessageBox.Show(message, @"Заполнены не все поля", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        #region Event handler'ы
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            bool useProxy = useProxyCB.Checked;
+            bool useCookie = useCookieCB.Checked;
+            string srcPageUrl = srcPageUrlTB.Text;
+            string proxyAddr = proxyAddrTB.Text;
+            string cookieStr = cookieTB.Text;
+            string localDir = localDirTB.Text;
+
+            //Проверки на пустые поля
+            if (!string.IsNullOrWhiteSpace(srcPageUrl) && !string.IsNullOrWhiteSpace(localDir))
+            {
+                if (useProxy)
+                {
+                    if (string.IsNullOrWhiteSpace(proxyAddr))
+                    {
+                        PartialInputError(@"Адрес прокси-сервера пуст!");
+                        return;
+                    }
+                } else
+                {
+                    proxyAddr = @"";
+                }
+                if (useCookie)
+                {
+                    if (string.IsNullOrWhiteSpace(cookieStr))
+                    {
+                        PartialInputError(@"Строка куков пуста!");
+                        return;
+                    }
+                }
+                else
+                {
+                    cookieStr = @"";
+                }
+                /*Если все условия выполнены - запустить скачивание, заблокировав на это
+                время кнопку*/
+                logTB.Clear(); //Очистить лог
+                ((Button)sender).Enabled = false;
+                DownloadPictures(srcPageUrl, useProxy, proxyAddr, useCookie, cookieStr,
+                    localDir);
+                ((Button)sender).Enabled = true;
+                progressBar.Value = 0; //Сбросить прогресс
+            } else
+            {
+                PartialInputError(@"Адрес конечной директории/адрес исходной страницы пуст");
+            }
+
+        }
+
+        private void clearSrcPageBtn_Click(object sender, EventArgs e)
+        {
+            srcPageUrlTB.Clear();
+            srcPageUrlTB.Focus();
+        }
+
+        private void clearProxyAddrBtn_Click(object sender, EventArgs e)
+        {
+            proxyAddrTB.Clear();
+            proxyAddrTB.Focus();
+        }
+
+        private void useCookieCB_CheckedChanged(object sender, EventArgs e)
+        {
+            bool enable = ((CheckBox)sender).Checked;
+            cookieTB.Enabled = enable;
+        }
+
+        private void useProxyCB_CheckedChanged(object sender, EventArgs e)
+        {
+            bool enable = ((CheckBox)sender).Checked;
+            proxyAddrTB.Enabled = enable;
+            clearProxyAddrBtn.Enabled = enable;
+        }
+
+        private void pickDirBtn_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDlg.ShowDialog() == DialogResult.OK &&
+                !string.IsNullOrWhiteSpace(folderBrowserDlg.SelectedPath))
+            {
+                string path = folderBrowserDlg.SelectedPath;
+                if (!path.EndsWith(@"\")) path += @"\"; //Добавить слеш в конце, если его нет
+                localDirTB.Text = path;
+            }
+        }
+
+        private void cookieTB_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.A)
+            {
+                ((TextBox)sender).SelectAll();
+            }
+        }
+
+        private void MainWindow_HelpButtonClicked(object sender, CancelEventArgs e)
+        {
+            MessageBox.Show(
+                "Автоматическое скачивание пикч по ссылкам с web-страницы.\n" +
+                "Автор: iw0rm3r",
+                @"О программе",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+            e.Cancel = true; //Отключить курсор мыши с вопросительным знаком
+        }
+        #endregion
+    }
+}
